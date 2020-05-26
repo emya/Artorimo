54b4b380940b308b68aded6da49bfbcde1fd1a83
emya <e.ayada810004@gmail.com>
2020-05-26 02:42:02 +0100

    add sidemenu to test

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 import {auth} from "../actions";
 import Header from './Header';
 import Footer from './Footer';
+import SideMenu from './SideMenu';
 
 import '../css/style.scss';
 
@@ -11,10 +12,22 @@ class Test extends Component {
 
   render() {
     return (
-      <div>
+  <div>
+    <Header />
 
-        <Footer />
-      </div>
+    <div class="menu">
+      <a href="/how-it-works">How It Works</a>
+      <a href="/about">About Us</a>
+      <a href="/contact-us">Contact Us</a>
+    </div>
+
+    <div class="wrapper clearfix">
+      <SideMenu />
+
+    </div>
+
+    <Footer />
+  </div>
     )
   }
 }
