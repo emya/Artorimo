9e7159dc46cdd4efdcfcff260f194b8158794eb9
chibee1 <cikuyama@gmail.com>
2020-05-30 16:44:19 -0400

    portfolio page and made it responsive

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -24,6 +24,50 @@ class Test extends Component {
     <div class="wrapper clearfix">
       <SideMenu />
 
+      <div class="illustrator-list port placeholder">
+      <h2>My Portfolio</h2>
+
+      <div class="profile-left">
+          <img class="portfolio-pic" src={require('../img/portrait.png')}/>
+      </div>
+
+      <div class="profile-right">
+        <p class="user-name">Chiaki Takahashi</p>
+        <p>Illustrator</p>
+      </div>
+
+        <ul class="port-list">
+          <li><div class="illustrator">
+            <img src={require('../img/portrait.png')}/>
+            </div></li>
+          <li><div class="illustrator">
+            <img src={require('../img/portrait.png')}/>
+            </div></li>
+          <li><div class="illustrator">
+            <img src={require('../img/portrait.png')}/>
+            </div></li>
+          <li><div class="illustrator">
+            <img src={require('../img/portrait.png')}/>
+            </div></li>
+          <li><div class="illustrator">
+            <img src={require('../img/portrait.png')}/>
+            </div></li>
+          <li><div class="illustrator">
+            <img src={require('../img/portrait.png')}/>
+            </div></li>
+          <li><div class="illustrator">
+            <img src={require('../img/portrait.png')}/>
+            </div></li>
+          <li><div class="illustrator">
+            <img src={require('../img/portrait.png')}/>
+            </div></li>
+          <li><div class="illustrator">
+            <img src={require('../img/portrait.png')}/>
+            </div></li>
+
+        </ul>
+      </div>
+
     </div>
 
     <Footer />
