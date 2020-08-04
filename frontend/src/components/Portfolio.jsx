6cf901537e500d62d109feb136f456a6949d1cbc
chibee1 <cikuyama@gmail.com>
2020-08-04 16:32:51 -0400

    fixed pic sizes

--- frontend/src/components/Portfolio.jsx
+++ frontend/src/components/Portfolio.jsx
@@ -29,96 +29,96 @@ class Portfolio extends Component {
       </div>
 
       {this.props.portfolio && this.props.portfolio.myportfolio && this.props.portfolio.myportfolio.map((portfolio) => (
-        <div class="illustrator-list port placeholder">
+        <div class="portfolio placeholder">
+        <div class="port-list ">
           <h2>Portfolio</h2>
 
 
+          <div class="profile-top">
           <div class="profile-left">
+          <div class="trim-wrap">
             {portfolio.profile.image && (
                 <img class="portfolio-pic" src={`https://${keys.AWS_BUCKET}.s3-us-west-2.amazonaws.com/profiles/${portfolio.profile.user.id}/${portfolio.profile.image}`} />
               )}
             {!portfolio.profile.image && (<img class="portfolio-pic" src={require('../img/default.png')} />)}
+          </div>
           </div>
 
           <div class="profile-right">
             {portfolio.profile.user_name && (<p class="user-name">{portfolio.profile.user_name}</p>)}
             <p>Illustrator</p>
           </div>
+          </div>
 
 
           <ul class="port-list">
-            <li><div class="illustrator">
+            <li>
               {portfolio.image0 ? (
                  <img src={`https://${keys.AWS_BUCKET}.s3-us-west-2.amazonaws.com/portfolios/${portfolio.profile.user.id}/${portfolio.image0}`} />
                   ) : (
-                 <img src={require('../img/portrait.png')}/>
+                 <img src={require('../img/default.png')}/>
               )}
-            </div></li>
+            </li>
 
-            <li><div class="illustrator">
+            <li>
               {portfolio.image1 ? (
                  <img src={`https://${keys.AWS_BUCKET}.s3-us-west-2.amazonaws.com/portfolios/${portfolio.profile.user.id}/${portfolio.image1}`} />
                   ) : (
-                 <img src={require('../img/portrait.png')}/>
+                 <img src={require('../img/default.png')}/>
               )}
-            </div></li>
+            </li>
 
-            <li><div class="illustrator">
+            <li>
               {portfolio.image2 ? (
                  <img src={`https://${keys.AWS_BUCKET}.s3-us-west-2.amazonaws.com/portfolios/${portfolio.profile.user.id}/${portfolio.image2}`} />
                   ) : (
-                 <img src={require('../img/portrait.png')}/>
+                 <img src={require('../img/default.png')}/>
               )}
-            </div></li>
+            </li>
 
-            <li><div class="illustrator">
+            <li>
               {portfolio.image3 ? (
                  <img src={`https://${keys.AWS_BUCKET}.s3-us-west-2.amazonaws.com/portfolios/${portfolio.profile.user.id}/${portfolio.image3}`} />
                   ) : (
-                 <img src={require('../img/portrait.png')}/>
+                 <img src={require('../img/default.png')}/>
               )}
-            </div></li>
+            </li>
 
-            <li><div class="illustrator">
+            <li>
               {portfolio.image4 ? (
                  <img src={`https://${keys.AWS_BUCKET}.s3-us-west-2.amazonaws.com/portfolios/${portfolio.profile.user.id}/${portfolio.image4}`} />
                   ) : (
-                 <img src={require('../img/portrait.png')}/>
+                 <img src={require('../img/default.png')}/>
               )}
-            </div></li>
+            </li>
 
-            <li><div class="illustrator">
+            <li>
               {portfolio.image5 ? (
                  <img src={`https://${keys.AWS_BUCKET}.s3-us-west-2.amazonaws.com/portfolios/${portfolio.profile.user.id}/${portfolio.image5}`} />
                   ) : (
-                 <img src={require('../img/portrait.png')}/>
+                 <img src={require('../img/default.png')}/>
               )}
-            </div></li>
+            </li>
 
-            <li><div class="illustrator">
+            <li>
               {portfolio.image6 ? (
                  <img src={`https://${keys.AWS_BUCKET}.s3-us-west-2.amazonaws.com/portfolios/${portfolio.profile.user.id}/${portfolio.image6}`} />
                   ) : (
-                 <img src={require('../img/portrait.png')}/>
+                 <img src={require('../img/default.png')}/>
               )}
-            </div></li>
+            </li>
 
-            <li><div class="illustrator">
+            <li>
               {portfolio.image7 ? (
                  <img src={`https://${keys.AWS_BUCKET}.s3-us-west-2.amazonaws.com/portfolios/${portfolio.profile.user.id}/${portfolio.image7}`} />
                   ) : (
-                 <img src={require('../img/portrait.png')}/>
+                 <img src={require('../img/default.png')}/>
               )}
-            </div></li>
+            </li>
 
-            <li><div class="illustrator">
-              {portfolio.image8 ? (
-                 <img src={`https://${keys.AWS_BUCKET}.s3-us-west-2.amazonaws.com/portfolios/${portfolio.profile.user.id}/${portfolio.image8}`} />
-                  ) : (
-                 <img src={require('../img/portrait.png')}/>
-              )}
-            </div></li>
+
           </ul>
+        </div>
         </div>
       ))}
     </div>
