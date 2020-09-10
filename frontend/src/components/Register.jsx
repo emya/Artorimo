24515430c06312ddc884274ba667e5d0f5267c67
emya <e.ayada810004@gmail.com>
2020-09-09 18:10:21 -0700

    update emagazine

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -87,7 +87,7 @@ class Login extends Component {
           <li><span class="todo">メールで仲介依頼をする</span>
             <ul>
               <li><span class="red">「海外クライアントからの仕事依頼が来たので対応して欲しい」「海外の企業やギャラリーに作品を売り込みたいので手伝って欲しい」</span>
-              <br/>そんな時は、<a href="mailto: ohcheestudio@gmail.com">ohcheestudio@gmail.com</a>までまずはご連絡ください！（ご相談は全て無料）</li>
+              <br/>そんな時は、<a href="mailto:ohcheestudio@gmail.com">ohcheestudio@gmail.com</a>までまずはご連絡ください！（ご相談は全て無料）</li>
               <li>1案件のみのご利用でももちろんOKです。</li>
             </ul>
           </li>
